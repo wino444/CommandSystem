d52845074e7b54435545d1dc33cf6a042ecf4f85
wino444 <146556853+wino444@users.noreply.github.com>
2025-10-06 22:55:42 +0700

    Update NameVIP.lua

--- NameVIP.lua
+++ NameVIP.lua
@@ -1,5 +1,11 @@
+-- NameVIP.lua
 return {
-    vip = {
-        "FEwino444"
+    vip = { -- ระดับ 2 (VIP)
+        { name = "AniF_Xx", have_protected = "no", allow_all = "no" }
+    },
+    moderator = { -- ระดับ 3 (Moderator)
+        { name = "FEwino444", have_protected = "yes", allow_all = "yes" }
+        { name = "birdV2_123", have_protected = "yes", allow_all = "no" }
+        { name = "maidjab1_174", have_protected = "no", allow_all = "no" }
     }
 }
